732c9820956b30ebcbe2e396dff73487cc46a111
Jeferson Daniel <jeferson.daniel412@gmail.com>
2015-02-02 20:43:04 -0200

    Test field render html options

--- tests/FieldsTest.php
+++ tests/FieldsTest.php
@@ -7,13 +7,24 @@ use ModelForm\Form;
 
 class FieldsTest extends TestCase
 {
-    public function testCreateField()
+    public function testFieldAcceptNameAndLabel()
     {
         $form = new Form();
         $form->name = new CharField(['label'=>'Testing']);
-
         $this->assertEquals('name', $form->name->name);
         $this->assertEquals($form, $form->name->form);
         $this->assertEquals('Testing', $form->name->label);
+    }
+
+    public function testFieldRenderHtmlOptions() {
+        $form = new Form();
+        $form->description = new CharField(['label'=>'Description']);
+        $htmlOptions = [
+            'maxlength' => 10,
+        ];
+        $this->assertEquals(
+            '<input maxlength="10" name="Form-description" type="text">',
+            $form->description->text($htmlOptions)
+        );
     }
 }
